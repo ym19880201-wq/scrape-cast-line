c8f62897a01e41b6c09c5c39ba574cb72b3d38cb
ym19880201-wq <ym19880201@gmail.com>
2026-04-12 05:25:22 +0900

    Update galaxy.py

--- sites/galaxy.py
+++ sites/galaxy.py
@@ -1,1 +1,17 @@
+from common import fetch, is_time
+import re
 
+
+def parse(url):
+    title, lines = fetch(url)
+
+    name = ""
+    shifts = []
+
+    name = title.split("/")[0].strip()
+
+    for i in range(len(lines) - 1):
+        if re.match(r"\d+/\d+\(.+\)", lines[i]) and is_time(lines[i + 1]):
+            shifts.append(lines[i])
+
+    return name, list(dict.fromkeys(shifts))
