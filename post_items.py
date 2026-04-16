5e58899302fda6844e3559db53676842b307f830
ym19880201-wq <ym19880201@gmail.com>
2026-04-17 06:19:30 +0900

    Update　Create post_items.py

---
+++ post_items.py
@@ -0,0 +1,93 @@
+POST_ITEMS = [
+    {
+        "shop_name": "エグゼ",
+        "cast_name": "のあ",
+        "title": "10代の名器持ち　グラインド腰使いのエロエロ騎乗位　初回GHR",
+        "url": "https://wakust.com/hagetaka1234/1221890/",
+    },
+    {
+        "shop_name": "ほしふるスパ",
+        "cast_name": "あやの",
+        "title": "マイナー店舗の２０歳セラピと初回無課金GHR",
+        "url": "https://wakust.com/hagetaka1234/1310303/",
+    },
+    {
+        "shop_name": "クライマックス東岡崎",
+        "cast_name": "くるみ❤️オススメ度120%",
+        "title": "愛知郊外２０代中盤セラピ　全身リップからの初回NS　未だに無名",
+        "url": "https://wakust.com/hagetaka1234/1290191/",
+    },
+    {
+        "shop_name": "トリハダスパ",
+        "cast_name": "天音 かんな",
+        "title": "１０代のロケットFカップと初回NS",
+        "url": "https://wakust.com/hagetaka1234/1298447/",
+    },
+    {
+        "shop_name": "カリーナ",
+        "cast_name": "愛莉（あいり）",
+        "title": "いっぱいだして♡　２０代前半激カワセラピと初回NN",
+        "url": "https://wakust.com/hagetaka1234/1367831/",
+    },
+    {
+        "shop_name": "フェリシア",
+        "cast_name": "桃瀬ゆいか",
+        "title": "２０代前半低身長ロリっ子と初回NS　最後は立ちバックでFINISH",
+        "url": "https://wakust.com/hagetaka1234/1397405/",
+    },
+    {
+        "shop_name": "ハニエル",
+        "cast_name": "ももこ",
+        "title": "初回ソファからのイチャイチャ初回GHR　２０代前半セラピの性欲あなどれません",
+        "url": "https://wakust.com/hagetaka1234/1603688/",
+    },
+    {
+        "shop_name": "セラトピア",
+        "cast_name": "ゆうか",
+        "title": "ドエロボディ、HR大好き２０歳セラピストと初回GHR",
+        "url": "https://wakust.com/hagetaka1234/1509522/",
+    },
+    {
+        "shop_name": "トリハダスパ",
+        "cast_name": "水嶋 わかな",
+        "title": "愛嬌Max　マッサージもエロも完璧　初回FからのいちゃいちゃNS",
+        "url": "https://wakust.com/hagetaka1234/1576408/",
+    },
+    {
+        "shop_name": "ギャラクシー",
+        "cast_name": "ゆり",
+        "title": "峰不二子スタイル　ハーフ美人F乳セラピと初回NS",
+        "url": "https://wakust.com/hagetaka1234/1611837/",
+    },
+    {
+        "shop_name": "ギャラクシー",
+        "cast_name": "れい",
+        "title": "２０代後半褐色どMギャルと初回NN",
+        "url": "https://wakust.com/hagetaka1234/1492639/",
+    },
+    {
+        "shop_name": "C-MOON",
+        "cast_name": "やみ",
+        "title": "小松菜奈似のマイナー店舗セラピと初回GHR",
+        "url": "https://wakust.com/hagetaka1234/1612298/",
+    },
+    {
+        "shop_name": "リゼクシー",
+        "cast_name": "白空 ぽえむ",
+        "title": "２０代前半　F乳ドMセラピストと初回NS",
+        "url": "https://wakust.com/hagetaka1234/1653062/",
+    },
+]
+
+
+def get_post_item_map():
+    post_item_map = {}
+
+    for item in POST_ITEMS:
+        key = (item["shop_name"], item["cast_name"])
+        post_item_map[key] = {
+            "title": item["title"],
+            "url": item["url"],
+        }
+
+    return post_item_map
